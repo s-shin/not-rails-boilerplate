b64131e6efe383d9a20a4eab82e05ee8900c505e
Shintaro Seki <s2pch.luck@gmail.com>
2017-03-20 20:52:51 +0900

    wip

--- test/app/test_context.rb
+++ test/app/test_context.rb
@@ -10,5 +10,21 @@ module App
     test 'project_root' do
       assert((Pathname(Context.new.project_root) + 'lib').directory?)
     end
+
+    test 'db' do
+      assert Context.new.db_w.is_a? Sequel::Database
+      assert Context.new.db_r.is_a? Sequel::Database
+    end
+
+    class Foo
+      Context.new.inject self, :env
+    end
+
+    test 'inject' do
+      foo = Foo.new
+      assert_nothing_raised { foo.env }
+      foo.env = 'foo'
+      assert_equal 'foo', foo.env
+    end
   end
 end
